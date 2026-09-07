7e14844241d72b1d1a40390231bf3fe93034e98f
Homesman <isaachuayta2006@gmail.com>
2026-09-07 18:08:35 -0500

    feat(shared):  add valueobjects

--- src/shared/domain/model/uuid.js
+++ src/shared/domain/model/uuid.js
@@ -1,24 +1,18 @@
-import {v7 as uuidv7, validate as uuidValidate} from 'uuid';
+import { v7 as uuidv7, validate as uuidValidate } from 'uuid';
 
 /**
- *
- * @returns {string | Uint8Array}
- *
+ * Generates a new UUID (Universally Unique Identifier) using the UUID version 7 algorithm.
+ * @returns {string} A newly generated UUID string.
  */
-export function generateUUID() {
-
+export function generateUuid() {
     return uuidv7();
-
 }
 
 /**
- * Validates whether a given value is a valid UUID
- * @param value - The value to be validated as a UUID
- * @returns {boolean} True if the value given to the funcion is a valid UUID, false otherwise
+ * Validates whether a given value is a valid UUID.
+ * @param value - The value to be validated as a UUID.
+ * @returns {boolean} True if the value is a valid UUID, false otherwise.
  */
 export function validateUuid(value) {
-
     return uuidValidate(value);
-
-
 }
